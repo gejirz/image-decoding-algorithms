eed9c2673db8547db8cdb807dc8081c49a700af9
Victor Andrievskiy <102751928+gejirz@users.noreply.github.com>
2023-03-26 12:41:12 +0300

    Update JFIF_Decoding_Algorithms.cpp

--- JFIF_Decoding_Algorithms.cpp
+++ JFIF_Decoding_Algorithms.cpp
@@ -10,7 +10,7 @@ int i, j, u, v;
 double c1, c2, c3;
 
 std::array<std::array<unsigned char, 8>, 8> do_InverseDCT(std::vector<std::vector<int>> raw_matrix_vect){
-	for (i = 0; i < 8; ++i){
+	for (i = 0; i < 8; ++i){ // initializing the vectors
 			for (j = 0; j < 8; ++j){
 				raw_matrix[i][j] = raw_matrix_vect[i][j];
 				IDCT_matrix[i][j] = 0.0;
@@ -18,18 +18,18 @@ std::array<std::array<unsigned char, 8>, 8> do_InverseDCT(std::vector<std::vecto
 	}
 	c1 = 0.7071; // approx. pow(2, -0.5)
 	c2 = 0.7071;
-	for (i = 0; i < 8; ++i){
+	for (i = 0; i < 8; ++i){ // IDCT
 		for (j = 0; j<8; ++j){
 			if (j == 0) c2 = 0.7071;
 			else c2 = 1.0;
 			for (u = 0; u<8; ++u){
-				for (v = 0; v<8; ++v)
+				for (v = 0; v<8; ++v) //3.1416 is approx. pi
 					IDCT_matrix[u][v] += 0.25 * c1 * c2 * raw_matrix[i][j] * cos(((2 * u + 1) * i * 3.1416) / 16) * cos(((2 * v + 1) * j * 3.1416) / 16);
 			}
 		}
 		c1 = 1;
 	}
-	for (i = 0; i < 8; ++i){
+	for (i = 0; i < 8; ++i){ // clamping algorithm
 		for (j = 0; j < 8; ++j){
 			c3 = IDCT_matrix[i][j];
 			if (c3 > 127) out_matrix[i][j] = 255;
